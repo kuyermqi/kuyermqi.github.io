0132cac8f03cafaa755046638f39cc451aaca228
kuyermqi <liuwenkiii@gmail.com>
2022-04-29 16:57:13 +0800

    update

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,32 @@
-import { html } from './posts/index.md'
+import { useEffect, useState } from 'react'
+import menu from './menu.json'
+
+const posts = import.meta.glob('./posts/*.md')
 
 function App() {
-  return <div dangerouslySetInnerHTML={{ __html: html }}></div>
+  const [html, setHtml] = useState(null)
+  const [path, setPath] = useState(null)
+
+  useEffect(() => {
+    if (path !== null) {
+      import(path).then(res => console.log(res))
+    }
+  }, [path])
+
+  return (
+    <div>
+      <h1>kuyermqi's blog</h1>
+      <ul>
+        {menu.map(item => {
+          return (
+            <li key={item.path} onClick={() => setPath(item.path)}>
+              {item.name}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default App
---
+++ src/posts/build-a-personal-blog-with-vite-and-react-and-github-pages.md
@@ -0,0 +1,3 @@
+# 使用 Vite + React + Github Pages 搭建个人博客
+
+This is markdown test.
