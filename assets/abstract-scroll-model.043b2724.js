b81dc32e625e8ee1321c0eb4008da1c1b5cd30eb
kuyermqi <liuwenkiii@gmail.com>
2022-05-30 19:29:43 +0800

    remove old build

--- assets/abstract-scroll-model.043b2724.js
+++
@@ -1,2 +0,0 @@
-const t={},h=`<h1>\u62BD\u8C61\u6EDA\u52A8\u6A21\u578B</h1>
-`;export{t as attributes,h as html};
--- assets/building-a-personal-blog-with-vite-and-react-and-github-pages.45e25931.js
+++
@@ -1,5 +0,0 @@
-const t={},e=`<h1>\u4F7F\u7528 Vite + React + Github Pages \u642D\u5EFA\u4E2A\u4EBA\u535A\u5BA2</h1>
-<p>This is markdown test.</p>
-<pre><code class="language-javascript">const fn = x =&gt; x ** 2
-</code></pre>
-`;export{t as attributes,e as html};
